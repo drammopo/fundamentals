95df79366fc428e43c60b7756951e764a751d4ad
drammopo <drammopo@gmail.com>
2014-05-04 18:16:03 +0200

    Changed text reference to word

--- anagram.rb
+++ anagram.rb
@@ -1,12 +1,12 @@
 class Anagram
-  attr_reader :permutations, :text
+  attr_reader :permutations, :word
 
-  def initialize(text)
-    @text = text.downcase
-    @permutations = text.downcase.split("").permutation.to_a.map(&:join)
+  def initialize(word)
+    @word = word.downcase
+    @permutations = word.downcase.split("").permutation.to_a.map(&:join)
   end
 
   def match(possible_matches)
-    possible_matches.find_all{ |match| permutations.include?(match.downcase) && match.downcase != text }
+    possible_matches.find_all{ |match| permutations.include?(match.downcase) && match.downcase != word }
   end
 end
